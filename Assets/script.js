21b8a71ad30ccd1bb42e38f15064e353976b65b4
alesofia9 <alesofia09@gmail.com>
2023-10-05 19:09:55 -0500

    Updated JS code, fixed 401 error on API key.

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,6 @@
 const apiKey = '2adeaa2be6msh80980fcbeb1b49ap1eca83jsndd1d02df27c5';
-const apiUrl = 'rapidapi.com';
+const apiUrl = 'https://diagnosis.p.rapidapi.com/api/DDxItems/GetTests?AuthenticationID';
 
-const url = 'https://diagnosis.p.rapidapi.com/api/DDxItems/GetTests?AuthenticationID=' + apiKey;
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,32 +9,39 @@ const options = {
 	}
 };
 
-//Change to .then and get fetch to work //
-try {
-	const response = fetch(url, options);
-	const result =  response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+document.getElementById('symptom-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent page from refreshing
 
+  const formElements = document.forms['symptom-form'].elements['symptoms'];
+  const selectedSymptoms = Array.from(formElements)
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.value);
 
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-   
-    var form = document.getElementById('symptom-form');
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); 
-  
-      
-      var checkboxes = form.querySelectorAll('input[name="symptoms"]:checked');
-  
-      
-      checkboxes.forEach(function(checkbox) {
-        console.log(checkbox.value);
-      });
+  // Call the API to get medical information based on selected symptoms
+  fetch(`${apiUrl}?symptom=${selectedSymptoms.join(',')}`,options)
+    .then(response => response.json())
+    .then(data => {
+      // Display the medical information on the page
+      displayMedicalInfo(data);
+        console.log(response);
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
+});
+
+function displayMedicalInfo(data) {
+  const medicalInfo = document.getElementById('medicalInfo');
+  // Clear previous medical information
+  medicalInfo.innerHTML = '';
+
+  // Loop through the retrieved data and display it on the page
+  data.forEach((info) => {
+    const symptomName = info.name;
+    const symptomDescription = info.description;
+
+    const infoElement = document.createElement('results');
+    infoElement.innerHTML = `<h3>${symptomName}</h3><p>${symptomDescription}</p>`;
+    medicalInfo.appendChild(infoElement);
   });
+}
