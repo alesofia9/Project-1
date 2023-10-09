7dff8a2e79665a2d5dc9100246f6f3a8b038d5a0
Jo3yG <nonuser31@yahoo.com>
2023-10-08 21:56:18 -0500

    cleaned up code more

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,6 @@
 var nameGenerateEl = document.querySelector("#name-generate");
 var nameContainerEl = document.querySelector("#generateName");
 
-
-
 const url = 'https://baby-names-by-api-ninjas.p.rapidapi.com/v1/babynames';
 const options = {
 	method: 'GET',
@@ -36,45 +34,3 @@ var buttonClickHandler = function (event) {
     };
   
     nameGenerateEl.addEventListener('click', buttonClickHandler);
-
-
-    //const infoElement = document.createElement('results');
-    //infoElement.innerHTML = `<h3>${symptomName}</h3>`;
-    //medicalInfo.appendChild(infoElement);
-
-//document.getElementById('symptom-form').addEventListener('submit', function(event) {
-  //event.preventDefault(); // Prevent page from refreshing
-
- // const formElements = document.forms['symptom-form'].elements['symptoms'];
-  //const selectedSymptoms = Array.from(formElements)
-    //.filter((checkbox) => checkbox.checked)
-    //.map((checkbox) => checkbox.value);
-
-  // Call the API to get medical information based on selected symptoms
-  //fetch(`${apiUrl}?symptom=${selectedSymptoms.join(',')}`,options)
-    //.then(response => response.json())
-   // .then(data => {
-      // Display the medical information on the page
-     // displayMedicalInfo(data);
-      //  console.log(response);
-    //})
-    //.catch(error => {
-   //  console.error('Error:', error);
-   // });
-//});
-
-//function displayMedicalInfo(data) {
- //const medicalInfo = document.getElementById('medicalInfo');
-  // Clear previous medical information
- // medicalInfo.innerHTML = '';
-
-  // Loop through the retrieved data and display it on the page
-  //data.forEach((info) => {
-   // const symptomName = info.name;
-   // const symptomDescription = info.description;
-
-   // const infoElement = document.createElement('results');
-    //infoElement.innerHTML = `<h3>${symptomName}</h3><p>${symptomDescription}</p>`;
-    //medicalInfo.appendChild(infoElement);
-  //});
-//}
